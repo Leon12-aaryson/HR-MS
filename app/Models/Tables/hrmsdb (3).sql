2557ce0627c9818a45257e3ae14f69d2220e74e9
Norman-dev <normanmukama11@gmail.com>
2023-10-19 18:14:37 +0300

    Have rpeplaced the database file

--- app/Models/Tables/hrmsdb (3).sql
+++ app/Models/Tables/hrmsdb (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2023 at 11:42 AM
+-- Generation Time: Oct 19, 2023 at 05:03 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
